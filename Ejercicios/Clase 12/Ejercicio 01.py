8c53c77c45d71da529109fd0484a95b91fd65dab
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2023-02-02 21:24:39 -0300

    Clase 12

---
+++ Ejercicios/Clase 12/Ejercicio 01.py
@@ -0,0 +1,9 @@
+'''
+Representa mediante un diagrama de clases el siguiente escenario:
+Una aplicación necesita almacenar información sobre empresas, sus empleados y sus clientes. 
+Ambos se caracterizan por su nombre y edad y derivan de una clase llamada Persona.
+✔ Los  empleados  tienen  un  sueldo  bruto,  los  empleados  que  son  directivos  tienen  una 
+categoría, así como un conjunto de empleados subordinados.
+✔ De los clientes además se necesita conocer su teléfono de contacto.
+✔ La aplicación necesita mostrar los datos de empleados y clientes.
+'''
