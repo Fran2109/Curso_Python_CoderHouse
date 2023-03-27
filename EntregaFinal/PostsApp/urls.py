6e6504c60c17c8a397ebd0215043dff05490d5c7
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2023-03-26 22:13:13 -0300

    Vista de Post Detalle

--- EntregaFinal/PostsApp/urls.py
+++ EntregaFinal/PostsApp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from PostsApp.views import ListaMisPosts
+from PostsApp.views import ListaMisPosts, PostDetalle
 
 app_name = 'PostsApp'
 
 urlpatterns = [
     path('listaMisPosts/', ListaMisPosts.as_view(), name="ListaMisPosts"),
+    path('listaPosts/<int:pk>/', PostDetalle.as_view(), name="PostDetalle"),
 ]
--- EntregaFinal/PostsApp/views.py
+++ EntregaFinal/PostsApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from PostsApp.models import Post
 
 # Create your views here.
@@ -11,3 +11,8 @@ class ListaMisPosts(LoginRequiredMixin, ListView):
     
     def get_queryset(self):
         return Post.objects.filter(author=self.request.user.profile).order_by("fecha_creacion", "titulo")
+
+class PostDetalle(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'post_detalle.html'
