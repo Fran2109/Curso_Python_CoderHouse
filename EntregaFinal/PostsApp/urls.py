11fc46872d179ffc7e8ba8732f289851eac1b626
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2023-03-27 19:28:40 -0300

    Lista de Posts

--- EntregaFinal/PostsApp/urls.py
+++ EntregaFinal/PostsApp/urls.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path
-from PostsApp.views import ListaMisPosts, PostDetalleMio
+from PostsApp.views import ListaMisPosts, ListaPosts, PostDetalleMio
 
 app_name = 'PostsApp'
 
 urlpatterns = [
     path('listaMisPosts/', ListaMisPosts.as_view(), name="ListaMisPosts"),
+    path('ListaPosts/', ListaPosts.as_view(), name="ListaPosts"),
     path('postDetalleMio/<int:pk>/', PostDetalleMio.as_view(), name="PostDetalleMio"),
 ]
--- EntregaFinal/PostsApp/views.py
+++ EntregaFinal/PostsApp/views.py
@@ -11,14 +11,14 @@ class ListaMisPosts(LoginRequiredMixin, ListView):
     template_name = 'lista_mis_posts.html'
     
     def get_queryset(self):
-        return Post.objects.filter(author=self.request.user.profile).order_by('fecha_creacion', "titulo")
+        return Post.objects.filter(author=self.request.user.profile).order_by('-fecha_creacion', "titulo")
 
 class PostDetalleMio(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detalle_mio.html'
     
     def get_context_data(self, *args, **kwargs):
-        posts = Post.objects.filter(author=self.request.user.profile).order_by('fecha_creacion', "titulo")
+        posts = Post.objects.filter(author=self.request.user.profile).order_by('-fecha_creacion', "titulo")
         post = get_object_or_404(Post, id=self.kwargs['pk'])
         post_index = list(posts).index(post)
         anterior_post = None
@@ -33,4 +33,10 @@ class PostDetalleMio(LoginRequiredMixin, DetailView):
         context['posterior_post'] = posterior_post
         return context
 
-        
+class ListaPosts(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'lista_posts.html'
+    
+    def get_queryset(self):
+        return Post.objects.order_by('-fecha_creacion', "titulo")
